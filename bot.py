84b4586138b7d90e1a9f1e2f4badd70ec64da613
mycarxx10-netizen <mycarxxx10@gmail.com>
2026-09-11 22:47:05 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message, PollAnswer
 from aiogram.filters import Command
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8601813721:AAGzvfJP2nDeqzkNSpeb2yx9phvHXSeMKz4"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
